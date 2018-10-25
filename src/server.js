53b82b4856314ce9d37b35e4fc71eb02bfbc709f
sachinjadhav <sachin.jadhav12@wipro.com>
2018-10-25 22:30:00 +0100

    port

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3002, ()=> {
+const PORT= process.env.PORT;
+app.listen( PORT || 3000, ()=> {
   console.log('app is running on port 3002');
 })
